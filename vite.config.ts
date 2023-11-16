f4075cbf40f79b16d255e59af1f85f9411d136b9
hiima <hiroki.iima@kaminashi.com>
2023-11-16 18:07:10 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,47 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
+
+const manifest: Partial<VitePWAOptions> = {
+  injectRegister: 'auto',
+  registerType: 'autoUpdate',
+  workbox: {
+    mode: 'injectManifest',
+  },
+  devOptions: {
+    enabled: false,
+  },
+  manifest: {
+    name: 'Oak App',
+    short_name: 'Oak',
+    description: 'Oak is the pwa test app.',
+    icons: [
+      {
+        src: 'app_icon/icon-192.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      {
+        src: 'app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    start_url: 'index.html',
+    display: 'fullscreen',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    lang: 'ja',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        mode: 'injectManifest',
-      },
-      devOptions: {
-        enabled: false,
-      },
-      manifest: {
-        name: 'Oak App',
-        short_name: 'Oak',
-        description: 'Oak is the pwa test app.',
-        icons: [
-          {
-            src: 'app_icon/icon-192.png',
-            type: 'image/png',
-            sizes: '192x192',
-          },
-          {
-            src: 'app_icon/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'app_icon/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-        start_url: 'index.html',
-        display: 'fullscreen',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        lang: 'ja',
-      },
-    }),
-  ],
+  plugins: [react(), VitePWA(manifest)],
 });
