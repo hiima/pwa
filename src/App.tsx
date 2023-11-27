9ad4455a35f8ef3a34b075cf7723c62906aa479c
hiima <hiroki.iima@kaminashi.com>
2023-11-27 11:28:09 +0900

    一定間隔で更新をチェックするように

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,42 @@
-import { useEffect, useState } from 'react';
-import { register } from 'register-service-worker';
-import './App.css';
+import { useState } from "react";
+import { useRegisterSW } from "virtual:pwa-register/react";
+import "./App.css";
+
+const intervalMS = 1 * 1000;
 
 function App() {
-  const [message, setMessage] = useState('No message.');
+  const [message, setMessage] = useState("No message.");
 
-  useEffect(() => {
-    register('/sw.js', {
-      registrationOptions: { scope: './' },
-      ready() {
-        setMessage('Service worker is active.');
-      },
-      registered() {
-        setMessage('Service worker has been registered.');
-      },
-      cached() {
-        setMessage('Content has been cached for offline use.');
-      },
-      updatefound() {
-        setMessage('New content is downloading.');
-      },
-      updated() {
-        setMessage('New content is available; please refresh.');
-      },
-      offline() {
-        setMessage(
-          'No internet connection found. App is running in offline mode.'
-        );
-      },
-      error(error) {
-        setMessage(`Error during service worker : ${error}`);
-      },
-    });
-  }, []);
+  const {
+    needRefresh: [needRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegisteredSW(swUrl, r) {
+      setMessage(`SW registration success ${swUrl}`);
+      if (r) {
+        // 一定間隔でサービスワーカーファイルの更新をチェックする
+        setInterval(async () => {
+          if (!(!r.installing && navigator)) return;
+          if ("connection" in navigator && !navigator.onLine) return;
+          console.log({ swUrl });
+          r.update();
+        }, intervalMS);
+      }
+    },
+    onRegisterError(error) {
+      setMessage(`SW registration error ${error}`);
+    },
+  });
+
+  const handleOk = () => {
+    updateServiceWorker(true);
+  };
 
   return (
     <>
-      <p>current version is `{import.meta.env.VITE_VERSION ?? 'undefined'}`</p>
+      <p>current version is `{import.meta.env.VITE_VERSION ?? "undefined"}`</p>
       <p>{message}</p>
+      {needRefresh && <button onClick={handleOk}>更新してください</button>}
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,45 @@
-import { defineConfig } from 'vite';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 const manifest: Partial<VitePWAOptions> = {
-  injectRegister: 'auto',
-  registerType: 'prompt',
+  injectRegister: "auto",
+  registerType: "prompt",
   workbox: {
-    mode: 'injectManifest',
-    globPatterns: ['**/*.{js,css,html,svg}'],
+    mode: "injectManifest",
+    globPatterns: ["**/*.{js,css,html,svg}"],
+    globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
   },
   devOptions: {
-    enabled: false,
+    enabled: true,
   },
   manifest: {
-    name: 'Oak App',
-    short_name: 'Oak',
-    description: 'Oak is the pwa test app.',
+    name: "Oak App",
+    short_name: "Oak",
+    description: "Oak is the pwa test app.",
     icons: [
       {
-        src: '/app_icon/icon-192.png',
-        type: 'image/png',
-        sizes: '192x192',
+        src: "/app_icon/icon-192.png",
+        type: "image/png",
+        sizes: "192x192",
       },
       {
-        src: '/app_icon/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/app_icon/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
       {
-        src: '/app_icon/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+        src: "/app_icon/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
     ],
-    start_url: 'index.html',
-    display: 'fullscreen',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    lang: 'ja',
+    start_url: "index.html",
+    display: "fullscreen",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    lang: "ja",
   },
 };
 
