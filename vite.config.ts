ad9c7d0492727ba720f84b9647f2397f2da07903
hiima <hiroki.iima@kaminashi.com>
2023-12-01 18:16:29 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,87 +1,88 @@
-import { defineConfig } from "vite";
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-const distDir = path.join(process.cwd(), "dist");
-const swDest = path.join(distDir, "serviceWorker.js"); // これで FCM の sw.js と被らない
-const cacheId = "oak-app";
+const distDir = path.join(process.cwd(), 'dist');
+const swDest = path.join(distDir, 'serviceWorker.js'); // これで FCM の sw.js と被らない
+const cacheId = 'oak-app';
 
 const manifest: Partial<VitePWAOptions> = {
-  injectRegister: "auto",
-  registerType: "prompt",
+  injectRegister: 'auto',
+  registerType: 'prompt',
   workbox: {
-    mode: "injectManifest",
-    globDirectory: "./dist/",
+    mode: 'injectManifest',
+    globDirectory: './dist/',
     globPatterns: [],
     // https://qiita.com/masato_makino/items/c85cb01d52632da42c03
     skipWaiting: true,
     cacheId,
     swDest,
+    importScripts: [path.join(distDir, 'firebase-messaging-sw.js')],
     runtimeCaching: [
-        {
-            urlPattern: /.+(\/|.html)$/,
-            handler: "NetworkFirst",
-            options: {
-                cacheName: cacheId + "-html-cache",
-                expiration: {
-                    maxAgeSeconds: 60 * 60 * 24 * 1,
-                },
-            },
-        },
       {
-            // js をキャッシュしているが、ファイル内容に変更があればビルド時にファイル名が変わるため、アップデートは想定通り行われるはず
-            urlPattern: /.+\.(js|css|woff)$/,
-            handler: "CacheFirst",
-            options: {
-                cacheName: cacheId + "-dependent-cache",
-                expiration: {
-                    maxAgeSeconds: 60 * 60 * 24 * 90,
-                },
-            },
+        urlPattern: /.+(\/|.html)$/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: cacheId + '-html-cache',
+          expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 1,
+          },
         },
-        {
-            urlPattern: /.+\.(png|gif|jpg|jpeg|svg)$/,
-            handler: "CacheFirst",
-            options: {
-                cacheName: cacheId + "-image-cache",
-                expiration: {
-                    maxAgeSeconds: 60 * 60 * 24 * 30,
-                },
-            },
+      },
+      {
+        // js をキャッシュしているが、ファイル内容に変更があればビルド時にファイル名が変わるため、アップデートは想定通り行われるはず
+        urlPattern: /.+\.(js|css|woff)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: cacheId + '-dependent-cache',
+          expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 90,
+          },
         },
-    ]
+      },
+      {
+        urlPattern: /.+\.(png|gif|jpg|jpeg|svg)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: cacheId + '-image-cache',
+          expiration: {
+            maxAgeSeconds: 60 * 60 * 24 * 30,
+          },
+        },
+      },
+    ],
   },
   devOptions: {
     enabled: true,
   },
   manifest: {
-    name: "Oak App",
-    short_name: "Oak",
-    description: "Oak is the pwa test app.",
+    name: 'Oak App',
+    short_name: 'Oak',
+    description: 'Oak is the pwa test app.',
     icons: [
       {
-        src: "/app_icon/icon-192.png",
-        type: "image/png",
-        sizes: "192x192",
+        src: '/app_icon/icon-192.png',
+        type: 'image/png',
+        sizes: '192x192',
       },
       {
-        src: "/app_icon/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
+        src: '/app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
       {
-        src: "/app_icon/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
+        src: '/app_icon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
       },
     ],
-    start_url: "index.html",
-    display: "fullscreen",
-    background_color: "#ffffff",
-    theme_color: "#000000",
-    lang: "ja",
+    start_url: 'index.html',
+    display: 'fullscreen',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    lang: 'ja',
   },
 };
 
